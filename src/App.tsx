9d087f170ce9bee6b4c94e09ed855d439653e867
Lingxi <lingxi.xing@screeningeagle.com>
2023-07-10 17:25:02 +0800

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
-            <p className="read-the-docs">BRO</p>
+            <p className="read-the-docs">hello</p>
         </>
     );
 }
