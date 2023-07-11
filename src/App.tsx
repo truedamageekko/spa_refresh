a93d83a9625a087040eb80e1b5c2360b03ba9fdf
Lingxi <lingxi.xing@screeningeagle.com>
2023-07-11 12:23:50 +0800

    added ping

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Modal } from "antd";
+import { useEffect } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const fetchData = async () => {
+        try {
+            const res = await fetch(
+                "https://main--meek-panda-62a9fa.netlify.app/api/hello"
+            );
+            const data = await res.json();
+            console.log(data);
+            const resVersion = res.headers.get("app-version") ?? "0";
+            if (resVersion > import.meta.env.VITE_VERSION) {
+                Modal.confirm({
+                    title: "New version of the BackOffice available!",
+                    okText: "Refresh",
+                    onOk: () => window.location.reload(),
+                });
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        console.log("load version: ", import.meta.env.VITE_VERSION);
+    }, []);
 
     return (
         <>
@@ -22,14 +45,8 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+                <button onClick={fetchData}>ping</button>
             </div>
-            <p className="read-the-docs">hi</p>
         </>
     );
 }
